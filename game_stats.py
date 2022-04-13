eaa15523da2e76899998d9fe23af05e9a58babae
AleksZaghloul <atomicka1@gmail.com>
2022-04-13 17:38:56 +0100

    Finished button

--- game_stats.py
+++ game_stats.py
@@ -4,7 +4,9 @@ class GameStats:
         #initialise stats
         self.settings = ai_game.settings
         self.reset_stats()
-        self.game_active = True
+
+        #start game inactive
+        self.game_active = False
 
     def reset_stats(self):
         #statistics that update during game
